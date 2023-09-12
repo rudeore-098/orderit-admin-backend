98c9887113efd831f1fd667820350c815f969457
jonghokim27 <joey0307@naver.com>
2023-09-12 15:53:36 +0900

    Initial commit

---
+++ src/main/java/kr/ac/ssu/orderit/OrderitBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ssu.orderit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderitBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/ssu/orderit/OrderitBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ssu.orderit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderitBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
